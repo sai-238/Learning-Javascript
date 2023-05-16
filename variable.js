a43f2fed0724d4a6f6d2b3cb38d72b346691663d
sai-238 <saiprasath238@gmail.com>
2023-05-17 00:01:14 +0530

    variables Done

--- variable.js
+++ variable.js
@@ -6,5 +6,11 @@ for (var index in x) {
   z += x[index];
 }
 z += y;
+const p1 = 1258
+const p2 = 1500
+let avg = p1 + p2 / 2
 
 console.log(z);
+console.log(avg);
+let tot = avg * z
+console.log(tot); 
