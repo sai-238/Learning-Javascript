b3f7ea830edc4ba06573823d58df14d16214ee32
sai-238 <saiprasath238@gmail.com>
2023-05-20 12:47:59 +0530

    if else done

---
+++ control_flow.js
@@ -0,0 +1,12 @@
+const num = prompt('Enter the number:')
+
+if (num > 0){
+    console.log('The number is positive')
+    document.write('The number is positive <br>')
+}
+else {
+    console.log('The number is negative')
+    document.write('The number is negative <br>')
+}
+console.log('The if statement is done')
+document.write('The if statement is done') 
