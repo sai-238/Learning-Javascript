421e51d1ca8bbf34c3bceaf99779131a21e21a79
sai-238 <saiprasath238@gmail.com>
2023-05-21 21:40:12 +0530

    While loop done

--- control_flow.js
+++ control_flow.js
@@ -1,12 +1,36 @@
-const num = prompt('Enter the number:')
+// const num = prompt('Enter the number:')
+// const n = 100
 
-if (num > 0){
-    console.log('The number is positive')
-    document.write('The number is positive <br>')
-}
-else {
-    console.log('The number is negative')
-    document.write('The number is negative <br>')
-}
-console.log('The if statement is done')
-document.write('The if statement is done') 
+// // if else loop
+// if (num > 0){
+//     console.log('The number is positive')
+//     document.write('The number is positive <br>')
+// }
+// else {
+//     console.log('The number is negative')
+//     document.write('The number is negative <br>')
+// }
+// console.log('The if statement is done')
+// document.write('The if statement is done <br>') 
+
+
+// // for loop 
+// let sum = 0 
+
+// for (let i = 0; i<=n; i++){
+//     sum += i
+// }
+// document.write('sum = ',sum)
+
+
+// While loop
+let sum = 0;
+
+let number = parseInt(prompt('Enter a number: '));
+
+do {
+    sum += number;
+    number = parseInt(prompt('Enter a number: '));
+} while(number >= 0)
+document.write(`The sum is ${sum}.`);
+
