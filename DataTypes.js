85f1cbfabb1c24e832ba382c49ce4efa74f77a73
sai-238 <saiprasath238@gmail.com>
2023-05-23 15:37:34 +0530

    DataTypes done

--- DataTypes.js
+++ DataTypes.js
@@ -1,3 +1,22 @@
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
 
 console.log(cars)
 console.log(person)
