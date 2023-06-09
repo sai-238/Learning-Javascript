23a9814f38a87119da75badee7d73c6996d797ae
sai-238 <saiprasath238@gmail.com>
2023-06-09 12:19:47 +0530

    exceptions and modeules done

--- em.js
+++ em.js
@@ -12,3 +12,41 @@ catch(error) {
     console.log('An error caught'); 
     console.log('Error message: ' + error);  
 }
+
+// try catch method with settimeout 
+
+setTimeout(function(){
+    try{
+        const num = 100 , dem = 'a'
+        console.log(num/dem)
+        console.log(a)
+    }
+    catch(error){
+        console.log('An error Caught')
+        console.log('Error message :' + error)
+    }
+
+}, 2000)
+
+// rethrow an expresion
+
+const number = 5;
+try {
+     // user-defined throw statement
+     throw new Error('This is the throw');
+
+}
+catch(error) {
+    console.log('An error caught');
+    if( number + 8 > 10) {
+
+        // statements to handle exceptions
+         console.log('Error message: ' + error); 
+        console.log('Error resolved');
+    }
+    else {
+        // cannot handle the exception
+        // rethrow the exception
+        throw new Error('The value is low');
+    }
+}
