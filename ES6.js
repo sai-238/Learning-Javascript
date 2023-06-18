f08bc5bc7dd13cd15370ebd6b55ae141c2df4822
sai-238 <saiprasath238@gmail.com>
2023-06-19 00:59:19 +0530

    upto javascript map

--- ES6.js
+++ ES6.js
@@ -59,3 +59,36 @@ const num1 = [10,20,30,40,50,60]
 
 add(...num1)
 
+// javascript default perameters
+
+let click = (f = 3 , g = 5) =>{
+    return f + g
+}
+
+console.log(click(5, 15))
+console.log(click(4))
+console.log(click())
+
+// Template literals or Template Strings
+
+const nam = 'Sai'
+console.log(`My name is ${nam}`)
+
+number = 11 + 10 
+
+console.log(`My age is : ${number}`)
+
+console.log(`${number < 18 ? 'Baby' : 'Adult'}`)
+
+// javascript map
+
+let map1 = new Map()
+
+let obj = {}
+map1.set(obj, {name : 'SAI', age : 20})
+
+console.log(map1)
+console.log(map1.get(obj))
+console.log(map1.has(obj))
+console.log(map1.delete(obj))
+console.log(map1)
